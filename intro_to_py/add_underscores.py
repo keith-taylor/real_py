efed9aa4c8da806463ee7d70fec858f0c5d00f1d
Keith Taylor <k-taylor@outlook.com>
2023-05-01 11:55:12 +0100

    adding factors.py file

--- intro_to_py/add_underscores.py
+++ intro_to_py/add_underscores.py
@@ -1,18 +1,11 @@
 def add_underscores(word):
     new_word = "_"
     for char in word:
-        new_word = char + "_"
+        new_word = new_word + char + "_"
     return new_word
 
 
 phrase = "hello"
 print(add_underscores(phrase))
-def add_underscores(word):
-    new_word = "_"
-    for char in word:
-        new_word = char + "_"
-    return new_word
 
 
-phrase = "hello"
-print(add_underscores(phrase))
---
+++ intro_to_py/factors.py
@@ -0,0 +1,27 @@
+
+initial_int = (input("Please enter an interger:> "))
+
+def calculate_factor_list(f_user_int):
+    factor_list = [1]
+    for i in range (2,f_user_int+1):
+        if f_user_int % i == 0:
+            factor_list.append(i)
+    if f_user_int == 1:
+        print("The only factor of one is one.")        
+    else:
+        last_item_in_list = factor_list.pop(-1)
+        print(f"The factors of {f_user_int} are: {', '.join(map(str,(factor_list)))} and {last_item_in_list}. ")
+
+try:
+    user_int = int(initial_int)
+    calculate_factor_list(user_int)
+except ValueError:
+    print("That is not an integer number. ")
+    
+
+
+
+
+
+
+
