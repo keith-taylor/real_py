4aab2ec3a6dd511fa455b1b1bb975a9f834819f1
keith-taylor <k-taylor@outlook.com>
2023-05-02 17:47:00 +0100

    testing ssh and github

---
+++ intro_to_py/rand2.py
@@ -0,0 +1,20 @@
+import random
+
+heads = 0
+tails = 0
+tosses = 10_000
+
+def coinflip(iterations):
+    head_count = 0
+    tail_count = 0
+    for i in range (iterations):
+        if random.randint(0,1) == 0:
+            head_count += 1
+        else:
+            tail_count += 1
+    return (head_count, tail_count) 
+
+heads, tails = coinflip(tosses)
+
+print(f"Heads count: {heads}")
+print(f"Tails count: {tails}")
