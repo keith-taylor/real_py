e7e537962f4f5f186d1a539e442538ac2fa0225b
Keith Taylor <k-taylor@outlook.com>
2023-05-02 16:42:42 +0100

    adding dicts.py

---
+++ intro_to_py/dicts.py
@@ -0,0 +1,8 @@
+my_dog = {
+    "name": "Rex",
+    "sex": "dog",
+    "year_born": "2020",
+    "month_born": "06"
+}
+
+print(f'{my_dog["name"]} is a {my_dog["sex"]} born in {my_dog["year_born"]}. ')
