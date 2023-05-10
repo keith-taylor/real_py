9681000db9b8d256f93334c915b0aba1767a2257
keith-taylor <k-taylor@outlook.com>
2023-05-10 12:35:07 +0100

    updated obj.py

--- intro_to_py/obj.py
+++ intro_to_py/obj.py
@@ -1,12 +1,15 @@
-class Point:
-    pass
+class Doggo:
+     species = "Canis familiaris"
+     def __init__(self, name, age):
+             self.name =  name
+             self.age = age
+             
+     # instance method
+     def description(self):
+             return f"{self.name} is {self.age} years old."
+     
+     # another sinatnce method
+     def speak(self, sound):
+             return f"{self.name} says {sound}!"
 
-class Dogg:
-    pass
-
-class Crew:
-    name  = 
-    age = 
-    role = 
-    crew_id = 
     
