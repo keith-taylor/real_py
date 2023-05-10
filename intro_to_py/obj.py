8cf2f5419a7e61e737e8d4f92200477cdeb8a474
keith-taylor <k-taylor@outlook.com>
2023-05-10 12:42:22 +0100

    updated obj.py

--- intro_to_py/obj.py
+++ intro_to_py/obj.py
@@ -1,15 +1,20 @@
 class Doggo:
-     species = "Canis familiaris"
-     def __init__(self, name, age):
-             self.name =  name
-             self.age = age
-             
-     # instance method
-     def description(self):
-             return f"{self.name} is {self.age} years old."
-     
-     # another sinatnce method
-     def speak(self, sound):
-             return f"{self.name} says {sound}!"
+    species = "Canis familiaris"
+    def __init__(self, name, age):
+        self.name =  name
+        self.age = age
+        
+    # instance method
+    def description(self):
+        return f"{self.name} is {self.age} years old."
 
-    
+    # another sinatnce method
+    def speak(self, sound):
+        return f"{self.name} says {sound}!"
+    #
+    def __str__(self):
+        return f"\n{self.name} is {self.age} years old and is a good dog! Yes he is!"
+
+Rex = Doggo("Rex", 7)
+
+print(Rex)    
