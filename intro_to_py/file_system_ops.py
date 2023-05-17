a19ede62537a47645017d72d361cd13c23610b90
keith-taylor <k-taylor@outlook.com>
2023-05-17 12:36:40 +0100

    updates to file_system_ops.py

--- intro_to_py/file_system_ops.py
+++ intro_to_py/file_system_ops.py
@@ -3,6 +3,22 @@ import pathlib
 pathlib.Path("/home/keith/Documents/Code/path_test.txt")
 
 my_home = (pathlib.Path.home())
-print(my_home)
 
-print(pathlib.Path.cwd())
+# print(pathlib.Path.cwd())
+
+paths_to_check = {}
+files_to_check = {}
+
+paths_to_check['check_path_1'] = my_home/"Documents"/"code"/"real_py"
+paths_to_check['check_path_2'] = my_home/"Documents"/"code"/"real_python"
+
+files_to_check['check_file_1'] = pathlib.Path.home()/"Documents"/"code"/"path_test.txt"
+files_to_check['check_file_2'] = pathlib.Path.home()/"Documents"/"code"/"silvia_path_test.txt" 
+
+
+for key, value in paths_to_check.items():
+    print (f"Checking {key}: {value} is a valid directory: {value.is_dir()}")
+
+for key, value in files_to_check.items():
+    print (f"Checking {key}: {value} is a valid file: {value.is_file()}")
+    
